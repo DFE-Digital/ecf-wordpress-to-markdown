f0146e644955a47a84a9b52dd5ab567f6a8b7bb3
Michal Kreft <michal.kreft@softwire.com>
2021-01-05 11:57:50 +0000

    Parse wordpress accordion component

--- parsers/handleWordpressComponents.js
+++ parsers/handleWordpressComponents.js
@@ -15,3 +15,28 @@ export function fixLinkButtons() {
     });
   };
 }
+
+export function fixAccordions() {
+  const accordionRegex = /<!-- wp:acf\/accordion ([\s\S]*?) \/-->/;
+  return (tree) => {
+    visit(tree, 'html', (node) => {
+      if (node.value) {
+        const match = accordionRegex.exec(node.value);
+        if (match) {
+          const jsonValue = JSON.parse(match[1]);
+          const { data } = jsonValue;
+          let sections = '';
+          let index = 0;
+          while (data[`section_${index}_heading`]) {
+            const heading = data[`section_${index}_heading`];
+            const summary = data[`section_${index}_summary`];
+            const content = data[`section_${index}_content`];
+            sections += `$Heading\n${heading}\n$EndHeading\n$Summary\n${summary}\n$EndSummary\n$Content\n${content}\n$EndContent\n`;
+            index += 1;
+          }
+          node.value = `$Accordion\n${sections}$EndAccordion`;
+        }
+      }
+    });
+  };
+}
